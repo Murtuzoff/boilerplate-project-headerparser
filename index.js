6d9ae82509127104bd65c4593c1719e5e7ba499e
Ruslan Murtuzoff <r.murtuzoff@gmail.com>
2024-04-20 17:26:11 +0000

    some fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static('public'));
@@ -15,6 +15,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-var listener = app.listen(process.env.PORT || 3000, function () {
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.header("Accept-Language"),
+    software: req.header("user-agent"),
+  });
+});
+
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
